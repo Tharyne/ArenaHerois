7545fd678da6867e42e0264e7933ddad6dcf6dfa
Tharyne <159277644+Tharyne@users.noreply.github.com>
2024-11-08 08:45:02 -0300

    Update Arqueiro.java

--- Arqueiro.java
+++ Arqueiro.java
@@ -1,5 +1,14 @@
 package ArenaDosHeróis;
 
-public class Arqueiro {
+public class Arqueiro extends Personagem{
+	
 
+	public Arqueiro (String nome) {
+		super (nome, 80, 20);
+	}
+	@Override
+	public void atacar (Personagem oponente) {
+		oponente.receberDano(this.getDanoBase());
+		System.out.println(getNome()+"disparou a flecha em"+oponente.getNome()+"a distancia");
+}
 }
